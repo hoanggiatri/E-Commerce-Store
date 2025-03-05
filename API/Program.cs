30d1c65b4e6b526abee16bf988691398e5bd0d0a
HGT <hoangtri21062003@gmail.com>
2025-03-05 10:21:06 +0700

    Home page UI

--- API/Program.cs
+++ API/Program.cs
@@ -25,7 +25,7 @@ app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
     .WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
-
+app.UseHttpsRedirection();
 app.MapControllers();
 
 try
