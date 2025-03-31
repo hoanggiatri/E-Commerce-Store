3d0b455a61ad4c1b7d13e12833688265c254f9cf
HGT <hoangtri21062003@gmail.com>
2025-03-31 20:31:00 +0700

    fixed ui

--- API/wwwroot/server/main.server.mjs
+++ API/wwwroot/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-EVO5QCDS.mjs";import"./chunk-HCEERKJN.mjs";import"./chunk-BYNKKX4U.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-FEYAPEDA.mjs";import"./chunk-PASQDMPI.mjs";import"./chunk-EMYZAIVN.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
